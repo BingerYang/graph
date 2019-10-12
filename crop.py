2f7d4dec9944235b0e8e9b4cf976116b549304c9
yangshujun <yangshujun@tezign.com>
2019-10-12 14:01:13 +0800

    修正剪切四周区域

--- crop.py
+++ crop.py
@@ -12,13 +12,13 @@ BINARY_BLACK = 0
 BINARY_USE_INV = BINARY_WHITE
 
 
-def crop_edge_area(image, color_range=None, alpha_range=None, is_run_crop=True):
+def crop_edge_area_by_rectangle(image, color_range=None, alpha_range=None, is_run_crop=True):
     """
     对于rgba图像获取 alpha 的非零区域
     对于rgb图像，获取非零或者255的区域
     :param image: PIL.ImageFile.ImageFile
-    :param color_range: 剪切边界的颜色区域
-    :param alpha_range: 剪切边界透明区域
+    :param color_range: 剪切边界的颜色区域，前开，后闭
+    :param alpha_range: 剪切边界透明区域，前开后闭
     :param is_run_crop: 是否扣除区域
     :return:
     """
@@ -36,15 +36,15 @@ def crop_edge_area(image, color_range=None, alpha_range=None, is_run_crop=True):
         ret, gray_image = cv2.threshold(crop_image, color_range[0], color_range[1], cv2.THRESH_BINARY_INV)
         points = np.argwhere(gray_image[:, :] == BINARY_WHITE)[:, ::-1]
         if points.size > 0:
+            x, y, w, h = cv2.boundingRect(points)
             if is_run_crop:
-                x, y, w, h = cv2.boundingRect(points)
                 roi = crop_image[y:y + h, x:x + w]
                 roi = Image.fromarray(roi)
-                return True, roi
+                return [x, y, w, h], roi
             else:
-                return True, image
+                return [x, y, w, h], image
         else:
-            return False, image
+            return None, image
 
     elif crop_image.shape[2] == 3:
         # RGB 模式的图形
@@ -52,15 +52,15 @@ def crop_edge_area(image, color_range=None, alpha_range=None, is_run_crop=True):
         ret, gray_image = cv2.threshold(gray_image, color_range[0], color_range[1], cv2.THRESH_BINARY_INV)
         points = np.argwhere(gray_image[:, :] == BINARY_WHITE)[:, ::-1]
         if points.size > 0:
+            x, y, w, h = cv2.boundingRect(points)
             if is_run_crop:
-                x, y, w, h = cv2.boundingRect(points)
                 roi = crop_image[y:y + h, x:x + w]
                 roi = Image.fromarray(roi)
-                return True, roi
+                return [x, y, w, h], roi
             else:
-                return True, image
+                return [x, y, w, h], image
         else:
-            return False, image
+            return None, image
     elif crop_image.shape[2] == 4:
         # RGBA 模式 可能存在透明
         alpha_range = alpha_range or (255 * 0.05, 255)
@@ -68,14 +68,13 @@ def crop_edge_area(image, color_range=None, alpha_range=None, is_run_crop=True):
         split_image = cv2.split(crop_image)
         points = np.argwhere((split_image[3] > alpha_range[0]) & (split_image[3] < alpha_range[1]))
         if points.size > 0:
+            y, x, h, w = cv2.boundingRect(points)
             if is_run_crop:
-                y, x, h, w = cv2.boundingRect(points)
                 roi = crop_image[y:y + h, x:x + w]
                 roi = Image.fromarray(roi)
-                return True, roi
+                return [x, y, w, h], roi
             else:
-                return True, image
+                return [x, y, w, h], image
         else:
-            return False, image
-
+            return None, image
 
